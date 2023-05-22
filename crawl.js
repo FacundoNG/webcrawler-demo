2e2eb1315e76824cdb4e6bde605a2c86b4120dbc
Facundo Gómez <facundo98n@gmail.com>
2023-05-22 14:05:02 -0300

    crawl single page implementation

--- crawl.js
+++ crawl.js
@@ -1,5 +1,29 @@
 const { JSDOM } = require('jsdom')
 
+async function crawlPage(currentURL) {
+    console.log(`Currently crawling: ${currentURL}`);
+
+    try {
+        const response = await fetch(currentURL);
+
+        if (response.status > 399) {
+            console.error(`Error in fetch with status code ${response.status} on page ${currentURL}`);
+            return;
+        }
+
+        const contentType = response.headers.get('content-type');
+
+        if (!contentType.includes("text/html")) {
+            console.error(`Non html response, content-type ${contentType} on page ${currentURL}`);
+            return;
+        }
+        console.log(await response.text());
+    } catch (err) {
+        console.error(`Error in fetch: ${err.message}, on page: ${currentURL}`);
+    }
+}
+
+
 function getURLsfromHTML(htmlBody, baseURL) {
     const urls = [];
     const dom = new JSDOM(htmlBody);
@@ -13,7 +37,7 @@ function getURLsfromHTML(htmlBody, baseURL) {
                 const urlObject = new URL(`${baseURL}${linkTag.href}`);
                 urls.push(`${urlObject.href}`)
             } catch (err) {
-                console.log(`failed parsing url: ${err.message}`)
+                console.error(`Failed parsing url: ${err.message}`)
             }
         } else {
             // absolute url
@@ -21,7 +45,7 @@ function getURLsfromHTML(htmlBody, baseURL) {
                 const urlObject = new URL(`${linkTag.href}`);
                 urls.push(linkTag.href)
             } catch (err) {
-                console.log(`failed parsing url: ${err.message}`)
+                console.error(`Failed parsing url: ${err.message}`)
             }
         }
     }
@@ -41,6 +65,7 @@ function normalizeURL(urlString) {
 
 
 module.exports = {
+    crawlPage,
     normalizeURL,
     getURLsfromHTML
 }
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hello node runtime');
+const { crawlPage } = require('./crawl');
+
+function main() {
+    if (process.argv.length < 3) {
+        console.error('No website provided');
+        process.exit(1);
+    } else if (process.argv.length < 3) {
+        console.error('This program only accepts 1 arg');
+        process.exit(1);
+    }
+
+    const baseURL = process.argv[2];
+
+    console.log(`Beginning crawl on: ${baseURL}`);
+    crawlPage(baseURL);
+}
+
+main();
